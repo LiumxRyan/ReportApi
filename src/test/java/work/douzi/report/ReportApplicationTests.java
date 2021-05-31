145dce1262548d2d9a452733d42d9b9a5c8da825
LiumxRyan <1258854493@qq.com>
2021-05-31 10:18:31 +0800

    初始化一个 SpringBoot 项目

---
+++ src/test/java/work/douzi/report/ReportApplicationTests.java
@@ -0,0 +1,13 @@
+package work.douzi.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
